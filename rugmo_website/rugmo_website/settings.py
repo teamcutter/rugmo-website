4b9a29c3a0d026ebcd319626d77b5614a993392c
teamcutter <ruslan.sibgatulin05@gmail.com>
2022-03-02 14:49:46 +0600

    deploy

--- rugmo_website/rugmo_website/settings.py
+++ rugmo_website/rugmo_website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vng$sqpwfpk%6vj7_xdi$q7xp=#43wtb2oktu%kic&%bbpfxgo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['apirugmo-website.herokuapp.com', '127.0.0.1']
 
