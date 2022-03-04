15e85a4f29ecb95aff7d8d8a01bb9637cf93ce3b
teamcutter <ruslan.sibgatulin05@gmail.com>
2022-03-04 23:06:01 +0600

    sadasdad

--- rugmo_website/rugmo_website/settings.py
+++ rugmo_website/rugmo_website/settings.py
@@ -135,9 +135,8 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
-MEDIA_ROOT =  os.path.join(BASE_DIR, 'static', 'media')
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
