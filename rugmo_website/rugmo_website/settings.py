e2b2847148684edf98b0e284ca5351b6eaf618d6
teamcutter <ruslan.sibgatulin05@gmail.com>
2022-03-04 23:11:47 +0600

    sdsdsd

--- rugmo_website/rugmo_website/settings.py
+++ rugmo_website/rugmo_website/settings.py
@@ -133,10 +133,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/media')
 STATIC_URL = '/static/'
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = [ 
+    os.path.join(BASE_DIR, 'static'),
+]
+
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'static/media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
