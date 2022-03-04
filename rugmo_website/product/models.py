028ae87d7cefc417a72ba8aa3c2ccdaf720e9694
teamcutter <ruslan.sibgatulin05@gmail.com>
2022-03-04 22:29:06 +0600

    changed

--- rugmo_website/product/models.py
+++ rugmo_website/product/models.py
@@ -44,18 +44,18 @@ class Product(models.Model):
 
     def get_image(self):
         if self.image:
-            return 'https://apirugmo-website.herokuapp.com' + self.image.url
+            return 'https://apirugmo-website.herokuapp.com:443' + self.image.url
         return ''
 
     def get_thumbnail(self):
         if self.thumbnail:
-            return 'https://apirugmo-website.herokuapp.com' + self.thumbnail.url
+            return 'https://apirugmo-website.herokuapp.com:443' + self.thumbnail.url
         else:
             if self.image:
                 self.thumbnail = self.make_thumbnail(self.image)
                 self.save()
 
-                return 'https://apirugmo-website.herokuapp.com' + self.thumbnail.url
+                return 'https://apirugmo-website.herokuapp.com:443' + self.thumbnail.url
             else:
                 return '' 
     
